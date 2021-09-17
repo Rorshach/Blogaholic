fdecc7bb3725e0fda04468687e264aa42274252e
Felix Tso <f.tso11200@gmail.com>
2021-09-17 14:11:32 -0700

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+
+const Home = () => {
+    const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                setBlogs(data)
+            });
+    }, []);
+
+    return ( 
+        <div className="home">
+            {blogs && <BlogList blogs={blogs} title="Behind the sliding doors" />}
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Blogaholic</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
