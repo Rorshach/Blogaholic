893f79b564683d33ba7f542c12007eab60c73146
Felix Tso <f.tso11200@gmail.com>
2022-02-01 16:15:48 -0800

    Blog page shows individual blog. Returned single blog on matching blog id. Disabled delete button.

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,19 +1,40 @@
 import { useParams } from "react-router";
-import useFetch from './useFetch';
+import {collection, doc, getDocs } from '@firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from "./fb";
 import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
     const { id } = useParams();
-    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const [blog, setBlog] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const docRef = collection(db, 'blogs');
+        getDocs(docRef)
+            .then((snapshot) => {
+                let blogs = []
+                snapshot.docs.forEach((doc) => {
+                    if(doc.id == id) {
+                        setBlog({...doc.data()});
+                        setIsPending(false);
+                        setError(null);
+                    }
+                })
+        });
+    });
+
+    // const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
     const history = useHistory();
 
-    const handleClick = () => {
-        fetch('http://localhost:8000/blogs/' + blog.id, {
-            method: 'DELETE'
-        }).then(() => {
-            history.push('/');
-        });
-    }
+    // const handleClick = () => {
+    //     fetch('http://localhost:8000/blogs/' + blog.id, {
+    //         method: 'DELETE'
+    //     }).then(() => {
+    //         history.push('/');
+    //     });
+    // }
 
     return ( 
         <div className="blog-details">
@@ -24,7 +45,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>    
                     <p>Written by: { blog.author }</p>
                     <div>{ blog.body }</div>
-                    <button onClick={handleClick}>Delete</button>
+                    {/* <button onClick={handleClick}>Delete</button> */}
                 </article>)}
         </div>
      );
